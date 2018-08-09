828bffd853dcb4aa73d259a433cbe3695b624b0c
zhangyl <zhangyulong@llzg.cn>
2018-08-09 14:49:56 +0800

    增加laravel-swoole

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,16 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return redirect("/home");
 });
 
 Auth::routes();
 
+//推特学习路由
 Route::get('/home', 'HomeController@index')->name('home');
-
-
 Route::post('tweet/save', 'twitter\PostController@store');
 Route::get('posts', 'twitter\PostController@index')->name('posts.index');
-
 Route::get('users/{user}/follow', 'users\UserController@follow')->name('user.follow');
 Route::get('users/{user}/unfollow', 'users\UserController@unfollow')->name('user.unfollow');
 Route::get('users/{user}', 'users\UserController@show')->name('user.show');
